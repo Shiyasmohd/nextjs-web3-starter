9153c59d6a0f45ad48077e3b8d59dbcdd1783cb6
shiyasmohd <shiyasjaseena124@gmail.com>
2023-06-14 05:59:25 +0530

    rainbow-kit & wagmi version updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
